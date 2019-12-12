4ebc57588117e7929b099847fce1bfe5e0ea7d18
zcp159 <330116856@qq.com>
2019-12-12 11:36:58 +0800

    第二次版本提交  添加日志功能及测试目录

--- start.py
+++ start.py
@@ -1,5 +1,6 @@
 import os
 import fnmatch
+import logging
 
 
 # 确定文件是否符合责规则
@@ -20,19 +21,36 @@ def is_special_file(root, guizes=["*"], liwaimulus=[]):
     for dangqianmulu, mulus, files in os.walk(os.path.abspath(root)):
         # 遍历返回的文件名
         for file in files:
+            file_juedui_lujing = os.path.join(os.path.abspath(dangqianmulu), file)
             # 判定文件名是否满足需要搜索的格式,如果符合则绝对路径放入生成器
             if is_file_math(file, guizes):
-                yield os.path.join(os.path.abspath(dangqianmulu), file)
+                logging.info(("{}  符合文件格式").format(file_juedui_lujing))
+                yield file_juedui_lujing
+            else:
+                logging.info(("{} 不符合文件格式").format(file_juedui_lujing))
         for d in liwaimulus:
             if d in mulus:
+                logging.debug(("{} 目录需要排除").format(os.path.join(dangqianmulu, d)))
                 mulus.remove(d)
 
 
+def get_logger():
+    rdir = os.path.dirname(os.path.abspath(__file__))  # 获取上级目录的绝对路径
+    log_dir = os.path.join(rdir, "record.log")  # 构建日志位置绝对路径
+    fh = logging.FileHandler(log_dir, "w", encoding='utf-8')  # 创建一个文件流并设置编码utf8
+    logger = logging.getLogger()  # 获得一个logger对象，默认是root
+    logger.setLevel(logging.DEBUG)  # 设置最低等级debug
+    fm = logging.Formatter("%(asctime)s --- %(message)s")  # 设置日志格式
+    logger.addHandler(fh)  # 把文件流添加进来，流向写入到文件
+    fh.setFormatter(fm)  # 把文件流添加写入格式
+
+
 def main():
+    get_logger()
     # 指定目录
-    mulu_1 = r"C:\Users\33011\Desktop\新建文件夹"
+    mulu_1 = r"D:\Clear cache\project_test\win10_main_p3\find_files\Test_case"
     # 指定文件格式
-    geshi_1 = ["*.txt", "*.jpg", "*log*"]
+    geshi_1 = ["*.txt"]
     liwaimulu_1 = ["bin", "conf"]
     # 获取满足条件的文件生成器
     result_files = is_special_file(mulu_1, geshi_1, liwaimulu_1)
